cb9f6fa30619e3de86153f147cd5d52bc0a5bbf7
kolbeyang <kky2806@rit.edu>
2025-03-01 20:04:00 -0500

    fix: basepath to see if it will work now

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   assetPrefix: process.env.NODE_ENV === "production" ? "/habits" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/habits" : "",
   images: {
     unoptimized: true,
   },
